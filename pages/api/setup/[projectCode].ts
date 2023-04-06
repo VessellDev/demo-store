e7abc0ba694816efa1a3bbfc649bc22471eb2893
Wyllyan Sapieginski <wyllyan@wyllyan.com>
2023-04-06 10:42:24 -0300

    Implement an API route to set the projectCode and the firebaseAuthTenantId in the cookies.

---
+++ pages/api/setup/[projectCode].ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'cookies-next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { projectCode, firebaseAuthTenantId } = req.query
+
+  if (!firebaseAuthTenantId) {
+    res.status(400).send(null)
+  }
+
+  setCookie('projectCode', projectCode, { req, res })
+  setCookie('firebaseAuthTenantId', firebaseAuthTenantId, { req, res })
+
+  res.redirect(307, '/')
+}
